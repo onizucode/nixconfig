83510bb59f1b7b0d576b76ccbd4f37ef2615751a
mikastiv <mikastiv@outlook.com>
2024-08-05 12:09:45 -0400

    update lock

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "mikastiv's flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOs/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
