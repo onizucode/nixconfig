b7c86b135d9eb3dec92164abf9b05a477d51415c
mikastiv <mikastiv@outlook.com>
2024-08-05 11:21:26 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "my flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
