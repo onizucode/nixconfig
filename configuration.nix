daac34d3ab7d92dfdfbb36f209aae12c39454ccc
khal38 <abdel.f38100@gmail.com>
2025-02-01 01:29:51 -0500

    save config

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,8 @@
     };
   };
 
-   
+
+
 #Depreciated
  #  hardware.opengl = {
  #   enable = true;
@@ -183,21 +184,6 @@
     nerd-fonts.jetbrains-mono
   ];
   
-
-
-   services.power-profiles-daemon.enable = false;
-   services.thermald.enable = true;
-
-
-   services.tlp = {
-      settings = {
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-  };
-};
-
 
   # Default programs
   programs = {
